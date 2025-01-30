5c4e6b731d91e362d0301f9b762a8d11002f9817
Matheus <matheus_benini@hotmail.com>
2025-01-30 13:18:33 -0300

    feat: add new table, migrations and updates in docker compose

---
+++ prisma/migrations/20250130155845_/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `Note` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NULL,
+    `content` VARCHAR(191) NOT NULL DEFAULT '',
+    `userId` VARCHAR(191) NOT NULL,
+    `visibility` VARCHAR(191) NOT NULL DEFAULT 'private',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `publicUrl` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Note_publicUrl_key`(`publicUrl`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Note` ADD CONSTRAINT `Note_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
